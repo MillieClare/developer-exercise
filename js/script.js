5367313dcf8eca984fb0b09a3421235d1ca03ba5
MillieClare <40922831+MillieClare@users.noreply.github.com>
2018-09-17 21:33:03 +0100

    for of loop for array, added parameter for number of recommendations

--- js/script.js
+++ js/script.js
@@ -1,23 +1,22 @@
-function showRecommendations(recommendationsDivId, recommendationsData) {
+function showRecommendations(recommendationsDivId, recommendationsData, maxRecommendations) {
     // recommendations div id passed in as string
     let recommendationsElement = $(recommendationsDivId);
-    console.log(recommendationsElement);
+    
     let recommendationsArray = recommendationsData['hits'].filter(recommendation => {
         return recommendation.hasOwnProperty('image');
-    }).slice(0,5);
+    }).slice(0, maxRecommendations);
 
-    for (let recommendationNumber = 0; recommendationNumber < recommendationsArray.length; recommendationNumber++) {
+   for(let recommendation of recommendationsArray){
         recommendationsElement.append('<div class="col-sm">' +
-            `<img src=${recommendationsArray[recommendationNumber].image.link} class="img-fluid" alt=${recommendationsArray[recommendationNumber].image.alt}>` +
-            `<div class="product_name">${recommendationsArray[recommendationNumber].product_name}</div>` +
-            `<div class="price">${recommendationsArray[recommendationNumber].price}</div>` +
+            `<img src=${recommendation.image.link} class="img-fluid" alt=${recommendation.image.alt}>` +
+            `<div class="product_name">${recommendation.product_name}</div>` +
+            `<div class="price">${recommendation.price}</div>` +
             '</div>');
-        //
     }
 }
 
 $(document).ready(function () {
     $.getJSON("data/recommendations.json", function(recommendationsJSON){
-        showRecommendations("#recommendations", recommendationsJSON);
+        showRecommendations("#recommendations", recommendationsJSON, 5);
     });
 });
