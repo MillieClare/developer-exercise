fec8f3e402a2185362f84c17534fb6130dc55dcb
MillieClare <40922831+MillieClare@users.noreply.github.com>
2018-09-17 21:27:34 +0100

    Data downloaded from JSON, filtered for image properties
    Limiting recommendations to 5

--- js/script.js
+++ js/script.js
@@ -1,62 +1,10 @@
-const recommendationsData = {
-    "_v": "15.6",
-    "_type": "product_search_result",
-    "count": 25,
-    "hits": [
-
-        {
-            "_type": "product_search_hit",
-            "currency": "GBP",
-            "image": {
-                "_type": "image",
-                "alt": "GREEN LEOPARD ASYMMETRIC DRESS Green Print",
-                "link": "https://www.warehouse.co.uk/on/demandware.static/-/Sites-WAREHOUSE/default/dw3f7660d4/images/hi-res/warehouse_03305824_1.jpg",
-                "title": "GREEN LEOPARD ASYMMETRIC DRESS Green Print"
-            },
-            "link": "https://www.warehouse.co.uk/s/Sites-Warehouse-UK-Site/dw/shop/v15_6/products/0330582406?refine=cgid%3Ddresses",
-            "price": 65,
-            "prices": {
-                "Original-1501": 65,
-                "Now-1501": 65
-            },
-            "product_id": "0330582406",
-            "product_name": "GREEN LEOPARD ASYMMETRIC DRESS",
-            "product_type": {
-                "_type": "product_type",
-                "variant": true
-            }
-        },
-
-        {
-            "_type": "product_search_hit",
-            "currency": "GBP",
-            "image": {
-                "_type": "image",
-                "alt": "CHECK MIDI SHIRT DRESS Navy",
-                "link": "https://www.warehouse.co.uk/on/demandware.static/-/Sites-WAREHOUSE/default/dwce82c99f/images/hi-res/warehouse_03274336_1.jpg",
-                "title": "CHECK MIDI SHIRT DRESS Navy"
-            },
-            "link": "https://www.warehouse.co.uk/s/Sites-Warehouse-UK-Site/dw/shop/v15_6/products/0327433608?refine=cgid%3Ddresses",
-            "price": 46,
-            "prices": {
-                "Original-1501": 46,
-                "Now-1501": 46
-            },
-            "product_id": "0327433608",
-            "product_name": "CHECK MIDI SHIRT DRESS",
-            "product_type": {
-                "_type": "product_type",
-                "variant": true
-            }
-        }
-    ]
-}
-
-function showRecommendations(recommendationsDivId) {
+function showRecommendations(recommendationsDivId, recommendationsData) {
     // recommendations div id passed in as string
     let recommendationsElement = $(recommendationsDivId);
     console.log(recommendationsElement);
-    let recommendationsArray = recommendationsData['hits'];
+    let recommendationsArray = recommendationsData['hits'].filter(recommendation => {
+        return recommendation.hasOwnProperty('image');
+    }).slice(0,5);
 
     for (let recommendationNumber = 0; recommendationNumber < recommendationsArray.length; recommendationNumber++) {
         recommendationsElement.append('<div class="col-sm">' +
@@ -69,5 +17,7 @@ function showRecommendations(recommendationsDivId) {
 }
 
 $(document).ready(function () {
-    showRecommendations("#recommendations");
+    $.getJSON("data/recommendations.json", function(recommendationsJSON){
+        showRecommendations("#recommendations", recommendationsJSON);
+    });
 });
